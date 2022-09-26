18df5b5d508aa46eacda340e8da6a49353f57fa4
Matthew Scheffel <mscheffel@gmail.com>
2022-09-26 10:09:03 -0300

    Improved interactivity

--- www/src/OutageChart.svelte
+++ www/src/OutageChart.svelte
@@ -30,7 +30,6 @@
             borderColor: color,
             backgroundColor: color,
             fill: false,
-            hidden: key == "NOVA SCOTIA"
         })
     });
 
@@ -45,7 +44,39 @@
                 legend: {
                     display: true,
                     position: 'bottom',
-                },     	
+                    onHover: function(event, legendItem) {
+                        document.getElementById("canvas").style.cursor = 'pointer';
+                    },
+                    onClick: function(e, legendItem) {
+                        var index = legendItem.datasetIndex;
+                        var ci = this.chart;
+                        var alreadyHidden = (ci.getDatasetMeta(index).hidden === null) ? false : ci.getDatasetMeta(index).hidden;
+
+                        ci.data.datasets.forEach(function(e, i) {
+                            var meta = ci.getDatasetMeta(i);
+
+                            if (i !== index) {
+                            if (!alreadyHidden) {
+                                meta.hidden = meta.hidden === null ? !meta.hidden : null;
+                            } else if (meta.hidden === null) {
+                                meta.hidden = true;
+                            }
+                            } else if (i === index) {
+                            meta.hidden = null;
+                            }
+                        });
+
+                        ci.update();
+                    },
+                },
+                tooltips: {
+                    custom: function(tooltip) {
+                    if (!tooltip.opacity) {
+                        document.getElementById("canvas").style.cursor = 'default';
+                        return;
+                    }
+                    }
+                }, 	
             },
             scales: {
                 x: {
